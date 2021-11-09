cbe59d112a4ec38e1ca88d8a1621f75467155159
Administrator <Administrator@NAS.itc03.edu>
2021-11-09 11:47:27 +0100

    Vezbe 3

--- Projekt-II/src/geometry/Point.java
+++ Projekt-II/src/geometry/Point.java
@@ -1,7 +1,35 @@
 package geometry;
 
 public class Point {
-	int x;
-	int y;
-	boolean selected;
+	private int x;
+	private int y;
+	private boolean selected;
+	
+	
+	public double distance(Point p) {
+		int dX = this.x - p.x;
+		int dY = this.y - p.y;
+		return Math.sqrt(dX*dX + dY*dY);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	
 }
--- Projekt-II/src/geometry/Test.java
+++ Projekt-II/src/geometry/Test.java
@@ -4,19 +4,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		Point p1 = new Point();
-		p1.x = 20;
-		p1.y = 30;
-		p1.selected = false;
-		System.out.println("X je: "+p1.x + " Y je: "+ p1.y + " Selected je: "+ p1.selected);
+		p1.setX(10);
+		p1.setY(30);
 		Point p2 = new Point();
-		p2.x = 20;
-		p2.y = 30;
-		p2.selected = false;
-		System.out.println("X je: "+p2.x + " Y je: "+ p2.y + " Selected je: "+ p2.selected);
+		p2.setX(20);
+		p2.setY(30);
 		System.out.println(p1 == p2);
 		int x =  20;
 		int y = 20;
 		System.out.println(x == y);
+		System.out.println(p1.distance(p2));
+		p1.getX();
+		Point p3 = new Point();
+		System.out.println(p3.getX());
+		Line l1 = new Line();
+		l1.length();
 	}
 
 }
